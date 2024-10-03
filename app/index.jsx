28f80987d803d021dcb9da1777693aee73d64f9c
Onario747 <onarigeorge013@gmail.com>
2024-10-03 17:41:56 +0100

    Implemented native wind and added splash screen for all devices

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className="flex items-center justify-center flex-1">
+      <Text className="text-xl text-green-500 font-bold">Auth</Text>
+    </View>
+  )
+}
+
+export default index
