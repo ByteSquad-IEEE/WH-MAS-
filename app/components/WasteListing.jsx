70f4799d39986da5df0ca5a380e55408189b7115
Onario747 <onarigeorge013@gmail.com>
2024-10-07 00:58:30 +0100

    updated waste listing

--- app/components/WasteListing.jsx
+++ app/components/WasteListing.jsx
@@ -3,17 +3,19 @@ import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
+  Image,
+  RefreshControl,
   StyleSheet,
   Text,
   View,
-  RefreshControl,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const WasteListing = () => {
   const [wasteList, setWasteList] = useState([]);
+  const [imageList, setImageList] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [refreshing, setRefreshing] = useState(false); // State for refreshing
+  const [refreshing, setRefreshing] = useState(false);
   const [error, setError] = useState(null);
 
   const getAllWasteList = async () => {
@@ -23,7 +25,6 @@ const WasteListing = () => {
         `${base_url}/wh-mas/api/products/get-all`
       );
 
-      // Convert the object of objects to an array
       if (response.data.message.success) {
         const wasteArray = Object.values(response.data.message.success);
         setWasteList(wasteArray);
@@ -38,11 +39,26 @@ const WasteListing = () => {
     }
   };
 
+  const getAllImages = async () => {
+    const base_url = "https://whmas-admin.vercel.app";
+    try {
+      const response = await axios.get(`${base_url}/wh-mas/api/images/get-all`);
+      if (response.data.message.success) {
+        const imageArray = Object.values(response.data.message.success);
+        setImageList(imageArray);
+      } else {
+        setError("No data available");
+      }
+    } catch (error) {
+      console.log("Error fetching Images:", error);
+    }
+  };
+
   useEffect(() => {
     getAllWasteList();
+    getAllImages();
   }, []);
 
-  // Function to refresh the list on pull
   const onRefresh = async () => {
     setRefreshing(true);
     await getAllWasteList();
@@ -67,30 +83,42 @@ const WasteListing = () => {
 
   const renderItem = ({ item }) => (
     <View style={styles.wasteCard}>
+      <Image
+        source={{ uri: imageList.map((img) => img.base64)[item.image1] }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{item.name}</Text>
-      <Text style={styles.infoText}>Price: ₦{item.price}</Text>
-      <Text style={styles.infoText}>Location: {item.location}</Text>
-      <Text style={styles.description}>{item.description}</Text>
+      <Text className="text-sm font-bold text-gray-500">
+        Location: {item.location}
+      </Text>
+      <Text style={styles.description} numberOfLines={2}>
+        {item.description}
+      </Text>
       <Text style={styles.infoText}>Date: {item.datestamp}</Text>
+      <Text className="text-lg font-bold text-gray-500">
+        Price: ₦{item.price}
+      </Text>
     </View>
   );
 
   return (
-      <FlatList
-        data={wasteList}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={renderItem}
-        contentContainerStyle={styles.listContainer}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      />
+    <FlatList
+      data={wasteList}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderItem}
+      numColumns={2}
+      contentContainerStyle={styles.listContainer}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      className="mt-4"
+    />
   );
 };
 
 const styles = StyleSheet.create({
   listContainer: {
-    padding: 16,
+    paddingVertical: 20,
   },
   centerContainer: {
     flex: 1,
@@ -98,24 +126,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   wasteCard: {
+    flex: 1,
     backgroundColor: "white",
     borderRadius: 8,
     padding: 16,
-    marginBottom: 16,
+    margin: 8,
     elevation: 2,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
   },
+  image: {
+    width: "100%",
+    height: 100,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
   title: {
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 8,
-  },
-  infoText: {
-    fontSize: 16,
-    marginBottom: 4,
   },
   description: {
     fontSize: 14,
